66b807c94eee02bea8d1e1e50aa2fee74a7139db
baokhang2003 <125106594+baokhang2003@users.noreply.github.com>
2023-04-26 23:06:36 +0700

    Add files via upload

---
+++ SinhVien.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication17;
+
+/**
+ *
+ * @author TH
+ */
+public class SinhVien {
+    private String maSV;
+    private String hoTen;
+
+    public SinhVien(String maSV, String hoTen) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    @Override
+    public String toString() {
+        return "Mã SV: "+ maSV+ "Họ tên: " + hoTen ;
+    }
+}
